998bb2d97d29cfd10da072d21bcbc9d5918e7872
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2022-10-20 17:41:07 +0800

    Create speech_to_text.py

---
+++ speech_to_text.py
@@ -0,0 +1,16 @@
+from multiprocessing.connection import Listener
+import speech_recognition as sr
+
+
+listener = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            command = command.replace('alexa', '')
+            print(command)
+except:
+    pass
