749f7f86efe34e70f1f2731e22ea1b2c746cc1d3
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2022-10-24 21:12:21 +0800

    fixed the conditional statement bug

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-import query
+import query as q
 def run_monikAI():
     # TODO fix the one time greeting each open
     # first_run = 0
     # while first_run == 0:
-    #     query.greeting()
+    #     q.greeting()
     #     first_run += 1
     #     if first_run == 1:
     #         break
 
-    command = query.query_input()
+    command = q.q_input()
     print(command + ' step 4 - monikAI')
+    if 'weather' in command or 'weather today' in command or "today's weather" in command or 'weather like today' in command or "weather for today" in command:
+        q.talk("getting today's weather forecast.")
+        q.weather_today()
 
-    if 'weather today' or 'weather like today' in command:
-        query.talk("getting today's weather forecast.")
-        query.weather_today()
-
-    elif 'play' or 'song' in command:
-        query.play_music()
+    elif 'play me a song' in command or 'play a song' in command or 'song' in command:
+        q.play_music()
 
 while True:
     run_monikAI()
