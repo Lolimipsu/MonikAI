8136c42a939437f453d29038806dceacaffb66e2
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2023-04-20 16:04:15 +0800

    cleaned main py

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import query as q
 
+# -----------------------------------------------------------------------
+# definitions
+# USE DOUBLE QUOTATION (NO '_' only "_")
+weather_prompt = ["weather", "weather today", "today's weather", "weather like today", "weather for today", "weather forecast"]
+play_song_prompt = ["play me a song", "play a song"]
+open_browser_prompt = ["open browser"]
+find_song_prompt = ["what song is this", "find this song", "name of song", "name of this song"]
+
+
+def contains_command(command_list, command):
+    return any(item in command for item in command_list)
+
+# -----------------------------------------------------------------------
+
 # q.greeting()
 
 def run_monikAI():
 
     command = q.query_input()
     print("> INPUT: " + command + ' \tIN monikAI main.py')
-    if 'weather' in command or 'weather today' in command or "today's weather" in command or 'weather like today' in command or "weather for today" in command:
+    if contains_command(weather_prompt, command):
         q.weather_today()
 
-    elif 'play me a song' in command or 'play a song' in command : #or 'song' in command
+    elif contains_command(play_song_prompt, command) : #or 'song' in command
         q.play_music()
 
-    elif 'open browser' in command:
+    elif contains_command(open_browser_prompt, command):
         q.open_site()
 
-    elif 'what song is this' in command or 'find this song' in command or 'name of song' in command:
+    elif contains_command(find_song_prompt, command):
         q.find_song()
     
     else:
